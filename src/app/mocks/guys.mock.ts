2d2342d6f5d8d6dc6e994d3b6541ae9aff424dbe
KatiaZd <katia.zouad@hotmail.fr>
2023-02-19 23:58:28 +0100

    Affichage homeComponent 1/2

--- src/app/mocks/guys.mock.ts
+++ src/app/mocks/guys.mock.ts
@@ -3,38 +3,44 @@ export type Genre = "Femme" | "Homme";
 export interface Guys {
     id: number;
     name: string;
+    description: string;
     genre: Genre;
 
 }
 
-export const GUYSLIST: Guys[] = [
+export const GUYS: Guys[] = [
     { 
         id: 1, 
         name: 'Mariam', 
+        description: "Tu es la grande gagnante !",
         genre: 'Femme' 
     },
 
     { 
         id: 2, 
         name: 'Patrick', 
+        description: "Tu es le grand gagnant !",
         genre: 'Homme' 
     },
 
     { 
         id: 3, 
         name: 'Yassir', 
+        description: "Tu es le grand gagnant !",
         genre: 'Homme' 
     },
 
     { 
         id: 4, 
         name: 'Monia', 
+        description: "Tu es la grande gagnante !",
         genre: 'Femme' 
     },
 
     { 
         id: 5, 
         name: 'Camille', 
+        description: "Tu es la grande gagnante !",
         genre: 'Femme' 
     },
 ];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Guys, GUYS } from 'src/app/mocks/guys.mock';
 
 @Component({
   selector: 'app-home',
@@ -8,15 +9,24 @@ import { Component } from '@angular/core';
 export class HomeComponent {
 
   //Tableau de personnes
-  personnes = ["Mariam", "Patrick", "Yassir", "Monia", "Camille"];
+  persones: Guys[] = GUYS;
+  result: Guys[] = GUYS;
+  isVisible = true;
 
   //Sélectionner une personne aléatoirement
-  personnesAleatoire : string[] = [];
+  select(){
+    this.result =this.persones;
+  }
 
   //Fonction qui choisit une personne aléatoirement. Math.random pour générer un nombre décimal. On * ce nombre par la longueur du tableau. Math.floor pour arrondir le résultat = index aléatoire du tableau. On retourne la personne à cet index.
   selectGuys() {
-    const select = Math.floor(Math.random() * this.personnes.length);
-    return this.personnes[select];
+    const select = Math.floor(Math.random() * this.persones.length);
+    return this.persones[select];
+  }
+
+  //Fonction qui filtre les personnes selon le genre
+  filterGuys(genre: string) {
+    this.result = this.persones.filter(personne => personne.genre === genre);
   }
 
 }
